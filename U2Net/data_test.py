60035edf154f41b09cc80df25497d423e57edca0
AjayMudhai <ajay.aj@iitgn.ac.in>
2021-07-05 00:51:37 +0530

    aa

---
+++ U2Net/data_test.py
@@ -0,0 +1,34 @@
+import os
+from os import listdir
+from PIL import Image
+
+
+def file_tester(src_pth,msk_pth):
+    a=[]
+    c=0
+    for root,dirs,files in os.walk(src_pth):
+        for file in files:
+            print('{}/{}'.format(c,len(files)))
+            c+=1
+            op=os.path.join(root,file)
+            mop=os.path.join(msk_pth,file)
+            try:
+                img = Image.open(op) # open the image file
+                img.verify() # verify that it is, in fact an image
+                img = Image.open(mop) # open the image file
+                img.verify()
+            except (IOError, SyntaxError) as e:
+                print('Bad file:', file)
+                os.remove(op)
+                os.remove(mop)
+                a.append(file)
+                print(a)
+
+
+src_pth='/datadrive/RemoveBG/removebg/U2Net/data/images'
+msk_pth='/datadrive/RemoveBG/removebg/U2Net/data/masks'
+file_tester(src_pth,msk_pth)
+
+    
+                
+            #os.remove(base_dir+"\\"+filename) (Maybe)
