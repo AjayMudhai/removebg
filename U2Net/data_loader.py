de541e37799acfac13e9ecc3da0477b679584f59
AjayMudhai <ajay.aj@iitgn.ac.in>
2021-07-05 01:03:10 +0530

    aa

--- U2Net/data_loader.py
+++ U2Net/data_loader.py
@@ -250,32 +250,35 @@ class SalObjDataset(Dataset):
 
 		# image = Image.open(self.image_name_list[idx])#io.imread(self.image_name_list[idx])
 		# label = Image.open(self.label_name_list[idx])#io.imread(self.label_name_list[idx])
+		try:
+			image = np.array(Image.open(self.image_name_list[idx]))
+			imname = self.image_name_list[idx]
+			imidx = np.array([idx])
 
-		image = np.array(Image.open(self.image_name_list[idx]))
-		imname = self.image_name_list[idx]
-		imidx = np.array([idx])
+			if(0==len(self.label_name_list)):
+				label_3 = np.zeros(image.shape)
+			else:
+				print((self.label_name_list[idx]))
+				label_3 = io.imread(self.label_name_list[idx])
 
-		if(0==len(self.label_name_list)):
-			label_3 = np.zeros(image.shape)
-		else:
-			print((self.label_name_list[idx]))
-			label_3 = io.imread(self.label_name_list[idx])
+			label = np.zeros(label_3.shape[0:2])
+			if(3==len(label_3.shape)):
+				label = label_3[:,:,0]
+			elif(2==len(label_3.shape)):
+				label = label_3
 
-		label = np.zeros(label_3.shape[0:2])
-		if(3==len(label_3.shape)):
-			label = label_3[:,:,0]
-		elif(2==len(label_3.shape)):
-			label = label_3
+			if(3==len(image.shape) and 2==len(label.shape)):
+				label = label[:,:,np.newaxis]
+			elif(2==len(image.shape) and 2==len(label.shape)):
+				image = image[:,:,np.newaxis]
+				label = label[:,:,np.newaxis]
 
-		if(3==len(image.shape) and 2==len(label.shape)):
-			label = label[:,:,np.newaxis]
-		elif(2==len(image.shape) and 2==len(label.shape)):
-			image = image[:,:,np.newaxis]
-			label = label[:,:,np.newaxis]
+			sample = {'imidx':imidx, 'image':image, 'label':label}
 
-		sample = {'imidx':imidx, 'image':image, 'label':label}
+			if self.transform:
+				sample = self.transform(sample)
 
-		if self.transform:
-			sample = self.transform(sample)
+			return sample
+		except:
+			pass
 
-		return sample
